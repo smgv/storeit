51e0922085608f2544a72bcf771111a2ffe2bd0c
smgv <gpatra1996@gmail.com>
2024-11-13 20:45:04 +0530

    auth layout added

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import React from "react"
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex min-h-screen">
+            <section className="bg-brand p-10">
+                <div>
+                    <Image src="/favicon.ico" alt="logo" width={16} height={16} className="h-auto" />
+                    <div className="space-y-5 text-white">
+                        <h1 className="h1">
+                            Manage your files the best ways
+                        </h1>
+                        <p className="body-1 ">This is a place where you store all your documents</p>
+                    </div>
+                </div>
+            </section>
+            {children}
+        </div>
+    )
+}
+
+export default Layout
